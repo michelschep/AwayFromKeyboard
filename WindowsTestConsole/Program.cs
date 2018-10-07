6fb23ed4ef3007ac7963220abe6efbd576eb1071
Michel Schep <michelschep@gmail.com>
2018-10-07 14:08:07 +0200

    Add simple test console to test HealthService

--- WindowsTestConsole/Program.cs
+++ WindowsTestConsole/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AwayFromKeyboard.Core.Services;
+using AwayFromKeyboard.WindowsInfrastructure;
 
 namespace WindowsTestConsole
 {
@@ -10,6 +12,12 @@ namespace WindowsTestConsole
     {
         static void Main(string[] args)
         {
+            var healthService = new HealthService(new Computer(), new BlinkStickNotifier(), new Clock());
+            healthService.Start();
+
+            Console.WriteLine("Health Service Running");
+            Console.ReadLine();
+            healthService.Stop();
         }
     }
 }
